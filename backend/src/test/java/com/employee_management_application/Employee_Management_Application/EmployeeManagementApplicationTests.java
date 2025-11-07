6c260bdd85891af076117e24cedd24fbbfa43a9b
lwaziT <61688164+lwaziT@users.noreply.github.com>
2025-11-07 22:17:42 +0200

    Refactor: Changed project file path

---
+++ backend/src/test/java/com/employee_management_application/Employee_Management_Application/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee_management_application.Employee_Management_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
